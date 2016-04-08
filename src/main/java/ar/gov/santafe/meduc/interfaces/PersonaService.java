ce94465454350788a11d7724920e04e6e62988f3
enorrmann@gmail.com <enorrmann@gmail.com>
2016-04-08 11:49:27 -0300

    creat3

--- src/main/java/ar/gov/santafe/meduc/interfaces/PersonaService.java
+++ src/main/java/ar/gov/santafe/meduc/interfaces/PersonaService.java
@@ -1,7 +1,9 @@
 package ar.gov.santafe.meduc.interfaces;
 
 import ar.gov.santafe.meduc.dto.SimpleDto;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -13,5 +15,10 @@ public interface PersonaService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public SimpleDto getPersona(@QueryParam("nombre") String nombre);
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public SimpleDto createPersona(SimpleDto persona);
 
 }
